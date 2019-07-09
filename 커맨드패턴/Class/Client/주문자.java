7b9f550c0a898f87df573a0ffdbea911e3bddfb0
부엉이 <o.o.wl.soft@gmail.com>
2019-07-09 18:48:28 +0900

    feat: Finish 커맨드패턴

--- 커맨드패턴/Class/Client/주문자.java
+++ 커맨드패턴/Class/Client/주문자.java
@@ -3,7 +3,11 @@ package 디자인패턴.커맨드패턴.Class.Client;
 public class 주문자 {
     String 이름;
 
-    public void 받다(Object any) {
-        System.out.println(any);
+    public 주문자(String 이름) {
+        this.이름 = 이름;
+    }
+
+    public void 받다(Object 제품) {
+        System.out.println(이름+"은 "+제품+"을 받았다");
     }
 }
